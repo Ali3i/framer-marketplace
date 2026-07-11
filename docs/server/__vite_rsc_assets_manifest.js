3c537272ba980b4127270664056e51656ec52086
Ali Alessa <ali@alialessa.info>
2026-07-11 22:23:47 +0300

    Publish static site to docs

---
+++ docs/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-D24Tzw1B.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-CnU3kW_o.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-BpSqSxVs.js",
+        "/assets/index-D24Tzw1B.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-D24Tzw1B.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-BpSqSxVs.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-BcxXVZqQ.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/index-D24Tzw1B.js",
+        "/assets/framework-BpSqSxVs.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-D24Tzw1B.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-BpSqSxVs.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-DyQ8mjS_.css"
+      ]
+    }
+  }
+}
